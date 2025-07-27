3aefbaa84c0cf841ce07094404eb65ed6a8b17dc
shrotriyadp <shrotriyadp@gmail.com>
2025-07-27 20:51:21 +0530

    Create streamlit_app.py
    Copied from my custom smoothies SiS App

---
+++ streamlit_app.py
@@ -0,0 +1,61 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+
+# Write directly to the app
+st.title(f":cup_with_straw: Example Streamlit App :cup_with_straw:")
+st.write(
+  """Choose the fruits you want in custom smoothie
+  """
+)
+
+#import streamlit as st
+
+#title = st.text_input('Movie title', 'Life of Brian')
+#st.write('The current movie title is', title)
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name of Smoothie will be:', name_on_order)
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')) 
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingrediendts_list = st.multiselect(
+     'Choose up to 5 ingredients:'
+     ,my_dataframe
+     ,max_selections=5
+      )
+
+if ingrediendts_list:
+    
+    
+    ingredients_string=''
+    for fruit_chosen in ingrediendts_list:
+        ingredients_string += fruit_chosen + ' '
+    st.write(ingredients_string)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+
+    st.write(my_insert_stmt)
+    st.stop()
+    time_to_insert = st.button('Submit Order')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+
+    #if ingredients_string:
+        #session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
+
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        og_dataset.merge(edited_dataset
+                     , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
